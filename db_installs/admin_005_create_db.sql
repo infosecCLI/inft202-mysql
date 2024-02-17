18decc5b89bfecf50859c8e8447e9a3dd886af61
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 15:23:26 -0500

    issue the command USE guttman_202db_assign4 5 pts

--- db_installs/admin_005_create_db.sql
+++ db_installs/admin_005_create_db.sql
@@ -1,4 +1,5 @@
 -- create database guttman_202db_assign4 5 pts
 DROP DATABASE IF EXISTS database guttman_202db_assign4;
 CREATE DATABASE IF NOT EXISTS database guttman_202db_assign4;
+-- issue the command USE guttman_202db_assign4 5 pts
 USE database guttman_202db_assign4;
