152adb331b30007db48ef1e1aaa0dd04bff554b7
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 15:38:28 -0500

    typo

--- db_installs/admin_005_create_db.sql
+++ db_installs/admin_005_create_db.sql
@@ -1,5 +1,8 @@
 -- create database guttman_202db_assign4 5 pts
-DROP DATABASE IF EXISTS database guttman_202db_assign4;
-CREATE DATABASE IF NOT EXISTS database guttman_202db_assign4;
+DROP DATABASE IF EXISTS guttman_202db_assign4;
+CREATE DATABASE IF NOT EXISTS guttman_202db_assign4;
 -- issue the command USE guttman_202db_assign4 5 pts
-USE database guttman_202db_assign4;
+USE guttman_202db_assign4;
+
+-- the end
+-- FLUSH PRIVILEGES;
