0e32aa170483f62d2b0c33d9df14b4b7c408754b
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 18:53:18 -0500

    set default roles

--- db_installs/six_new_users.sql
+++ db_installs/six_new_users.sql
@@ -4,9 +4,9 @@ USE six_new_users;
 
 -- create admin_001
 SELECT 'DROPPING USER admin_001' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_001'@'%';
+DROP USER IF EXISTS 'admin_001'@'localhost';
 SELECT 'ADDING USER admin_001' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_001'@'%'
+CREATE USER IF NOT EXISTS 'admin_001'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -21,9 +21,9 @@ SELECT 'ADDED USER admin_001' AS 'INSTALLATION COMPLETED';
 
 -- create admin_002
 SELECT 'DROPPING USER admin_002' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_002'@'%';
+DROP USER IF EXISTS 'admin_002'@'localhost';
 SELECT 'ADDING USER admin_002' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_002'@'%'
+CREATE USER IF NOT EXISTS 'admin_002'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -38,9 +38,9 @@ SELECT 'ADDED USER admin_002' AS 'INSTALLATION COMPLETED';
 
 -- create admin_003
 SELECT 'DROPPING USER admin_003' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_003'@'%';
+DROP USER IF EXISTS 'admin_003'@'localhost';
 SELECT 'ADDING USER admin_003' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_003'@'%'
+CREATE USER IF NOT EXISTS 'admin_003'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -55,9 +55,9 @@ SELECT 'ADDED USER admin_003' AS 'INSTALLATION COMPLETED';
 
 -- create admin_004
 SELECT 'DROPPING USER admin_004' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_004'@'%';
+DROP USER IF EXISTS 'admin_004'@'localhost';
 SELECT 'ADDING USER admin_004' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_004'@'%'
+CREATE USER IF NOT EXISTS 'admin_004'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -72,9 +72,9 @@ SELECT 'ADDED USER admin_004' AS 'INSTALLATION COMPLETED';
 
 -- create admin_005
 SELECT 'DROPPING USER admin_005' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_005'@'%';
+DROP USER IF EXISTS 'admin_005'@'localhost';
 SELECT 'ADDING USER admin_005' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_005'@'%'
+CREATE USER IF NOT EXISTS 'admin_005'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -89,9 +89,9 @@ SELECT 'ADDED USER admin_005' AS 'INSTALLATION COMPLETED';
 
 -- create admin_006
 SELECT 'DROPPING USER admin_006' AS 'INSTALLATION STARTED';
-DROP USER IF EXISTS 'admin_006'@'%';
+DROP USER IF EXISTS 'admin_006'@'localhost';
 SELECT 'ADDING USER admin_006' AS 'INSTALLATION PROGRESSING';
-CREATE USER IF NOT EXISTS 'admin_006'@'%'
+CREATE USER IF NOT EXISTS 'admin_006'@'localhost'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -126,17 +126,28 @@ GRANT SELECT ON employees.* TO read_only_employees_db;
 -- GRANT CREATE, DROP ON
 GRANT CREATE, DROP ON employees.* TO admin_user;
 GRANT CREATE, DROP ON classicmodels.* TO admin_user;
--- GRANT SHOW DATABASES ON
-GRANT SHOW DATABASES ON *.* TO admin_user;
+-- GRANT SHOW DATABASES ON *.*
+-- GRANT SHOW DATABASES ON *.* TO admin_user;
+-- GRANT ON guttman_202db_assign4 TO admin_user;
+GRANT ALL ON *.* TO admin_user;
 -- GRANT INSERT, UPDATE ON
 GRANT INSERT, UPDATE ON employees.employees TO app_user;
 
 -- grant roles to users
-GRANT 'admin_007' TO 'admin_006'@'%';
-GRANT 'read_only_classicmodels_db' TO 'admin_001'@'%', 'admin_003'@'%';
-GRANT 'read_only_employees_db' TO 'admin_002'@'%', 'admin_004'@'%';
-GRANT 'app_user' TO 'admin_006'@'%';
-GRANT 'admin_user' TO 'admin_005'@'%';
+GRANT 'admin_007' TO 'admin_006'@'localhost';
+GRANT 'read_only_classicmodels_db' TO 'admin_001'@'localhost', 'admin_003'@'localhost';
+GRANT 'read_only_employees_db' TO 'admin_002'@'localhost', 'admin_004'@'localhost';
+GRANT 'app_user' TO 'admin_006'@'localhost';
+GRANT 'admin_user' TO 'admin_005'@'localhost';
+
+-- set default roles AFTER drop, create, grant
+SET DEFAULT ROLE ALL TO
+'admin_001'@'localhost',
+'admin_002'@'localhost',
+'admin_003'@'localhost',
+'admin_004'@'localhost',
+'admin_005'@'localhost',
+'admin_006'@'localhost';
 
 FLUSH PRIVILEGES;
 -- remove FLUSH PRIVILEGES per you don’t need to run FLUSH PRIVILEGES command (this is effective for a long time already, please forget about it !),
