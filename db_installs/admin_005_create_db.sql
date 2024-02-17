8d81cfa96d0e435c350942bc2e816739c1e763d6
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 15:22:30 -0500

    create database guttman_202db_assign4 5 pts

---
+++ db_installs/admin_005_create_db.sql
@@ -0,0 +1,4 @@
+-- create database guttman_202db_assign4 5 pts
+DROP DATABASE IF EXISTS database guttman_202db_assign4;
+CREATE DATABASE IF NOT EXISTS database guttman_202db_assign4;
+USE database guttman_202db_assign4;
