3654bed89cdc68e7d82d8f47ce14b1b3381e62a2
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-16 21:59:45 -0500

    how did those typos happen

--- db_installs/six_new_users.sql
+++ db_installs/six_new_users.sql
@@ -2,78 +2,9 @@ DROP DATABASE IF EXISTS six_new_users;
 CREATE DATABASE IF NOT EXISTS six_new_users;
 USE six_new_users;
 
--- create rt_user
--- check if rt_user exists and drop if it does
-SELECT 'DROPPING USER rt_user' AS 'INSTALLATION STARTED';
-
-DROP USER IF EXISTS 'rt_user'@'%';
-
-SELECT 'ADDING USER rt_user' AS 'INSTALLATION PROGRESSING';
-
-CREATE USER 'rt_user'@'%'
-IDENTIFIED BY 'super_secret_password_911'
-PASSWORD EXPIRE INTERVAL 90 DAY
-PASSWORD HISTORY 5
-PASSWORD REUSE INTERVAL 365 DAY
-FAILED_LOGIN_ATTEMPTS 0
-PASSWORD_LOCK_TIME 0;
-
--- revoke all privileges and grant option from 'rt_user'@'localhost'
-REVOKE ALL, GRANT OPTION FROM 'rt_user'@'%';
-
--- grant ALL privileges on *.* to 'rt_user'@'localhost'
-GRANT ALL ON *.* TO 'rt_user'@'localhost';
-
--- select specific columns from mysql.user table
-SELECT user, show_db_priv, account_locked
-FROM mysql.user;
-
--- return informational message that indicates completion of user addition
-SELECT 'Completed adding rt_user' AS 'INSTALLATION DONE';
-
--- flush privileges to ensure changes take effect immediately
-FLUSH PRIVILEGES;
-
--- create brt_user
--- check if brt_user exists and drop if it does
-SELECT 'DROPPING USER brt_user' AS 'INSTALLATION STARTED';
-
-DROP USER IF EXISTS 'brt_user'@'%';
-
-SELECT 'ADDING USER brt_user' AS 'INSTALLATION PROGRESSING';
-
-CREATE USER 'brt_user'@'%'
-IDENTIFIED BY 'super_secret_password_911'
-PASSWORD EXPIRE INTERVAL 90 DAY
-PASSWORD HISTORY 5
-PASSWORD REUSE INTERVAL 365 DAY
-FAILED_LOGIN_ATTEMPTS 0
-PASSWORD_LOCK_TIME 0;
-
--- revoke all privileges and grant option from 'brt_user'@'localhost'
-REVOKE ALL, GRANT OPTION FROM 'brt_user'@'localhost';
-
--- grant ALL privileges on *.* to 'brt_user'@'localhost'
-GRANT ALL ON *.* TO 'brt_user'@'localhost';
-
--- return an informational message which indicates completion of installation
-SELECT 'COMPLETED' AS 'INSTALLATION DONE';
-
--- select specific columns from mysql.user table
-SELECT user, host, show_db_priv, account_locked
-FROM mysql.user;
-
--- flush privileges to ensure changes take effect immediately
-FLUSH PRIVILEGES;
-
--- create admin_007
-SELECT 'DROPPING USER admin_007' AS 'INSTALLATION STARTED';
-
-DROP USER IF EXISTS 'admin_007'@'localhost';
-
-SELECT 'ADDING USER admin_007' AS 'INSTALLATION PROGRESSING';
-
-CREATE USER IF NOT EXISTS 'admin_007'@'%'
+-- Create admin_001
+DROP USER IF EXISTS 'admin_001'@'%';
+CREATE USER 'admin_001'@'%'
 IDENTIFIED BY 'guttmanGrizzlies_2024'
 FAILED_LOGIN_ATTEMPTS 4
 PASSWORD_LOCK_TIME UNBOUNDED
@@ -81,18 +12,76 @@ PASSWORD EXPIRE INTERVAL 90 DAY
 PASSWORD HISTORY 5
 PASSWORD REUSE INTERVAL 365 DAY;
 
--- revoke all privileges and grant option from 'admin_007'@'%'
-REVOKE ALL, GRANT OPTION FROM 'admin_007'@'%';
+REVOKE ALL, GRANT OPTION FROM 'admin_001'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_001'@'%';
 
--- select specific columns from mysql.user table
-SELECT user, show_db_priv, account_locked
-FROM mysql.user;
+-- Create admin_002
+DROP USER IF EXISTS 'admin_002'@'%';
+CREATE USER 'admin_002'@'%'
+IDENTIFIED BY 'guttmanGrizzlies_2024'
+FAILED_LOGIN_ATTEMPTS 4
+PASSWORD_LOCK_TIME UNBOUNDED
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
 
--- return an informational message to indicate completion of user addition
-SELECT 'ADDED USER admin_007' AS 'INSTALLATION COMPLETED';
+REVOKE ALL, GRANT OPTION FROM 'admin_002'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_002'@'%';
 
--- grant SELECT privilege on classicmodels.* to 'admin_007'@'%'
-GRANT SELECT ON classicmodels.* TO 'admin_007'@'%';
+-- Create admin_003
+DROP USER IF EXISTS 'admin_003'@'%';
+CREATE USER 'admin_003'@'%'
+IDENTIFIED BY 'guttmanGrizzlies_2024'
+FAILED_LOGIN_ATTEMPTS 4
+PASSWORD_LOCK_TIME UNBOUNDED
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
+
+REVOKE ALL, GRANT OPTION FROM 'admin_003'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_003'@'%';
+
+-- create admin_004
+DROP USER IF EXISTS 'admin_004'@'%';
+CREATE USER 'admin_004'@'%'
+IDENTIFIED BY 'guttmanGrizzlies_2024'
+FAILED_LOGIN_ATTEMPTS 4
+PASSWORD_LOCK_TIME UNBOUNDED
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
+
+REVOKE ALL, GRANT OPTION FROM 'admin_004'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_004'@'%';
+
+-- create admin_005
+DROP USER IF EXISTS 'admin_005'@'%';
+CREATE USER 'admin_005'@'%'
+IDENTIFIED BY 'guttmanGrizzlies_2024'
+FAILED_LOGIN_ATTEMPTS 4
+PASSWORD_LOCK_TIME UNBOUNDED
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
+
+REVOKE ALL, GRANT OPTION FROM 'admin_005'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_005'@'%';
+
+-- create admin_006
+DROP USER IF EXISTS 'admin_006'@'%';
+CREATE USER 'admin_006'@'%'
+IDENTIFIED BY 'guttmanGrizzlies_2024'
+FAILED_LOGIN_ATTEMPTS 4
+PASSWORD_LOCK_TIME UNBOUNDED
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
+
+REVOKE ALL, GRANT OPTION FROM 'admin_006'@'%';
+GRANT SELECT ON classicmodels.* TO 'admin_006'@'%';
+
+FLUSH PRIVILEGES;
+
 /*
 -- create roles
 
