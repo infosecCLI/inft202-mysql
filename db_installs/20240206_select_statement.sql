6ea9c362810caf398d04d689b7ed7e436be74ad5
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 20:48:18 -0500

    CREATE and USE

---
+++ db_installs/20240206_select_statement.sql
@@ -0,0 +1,8 @@
+-- create database guttman_202db_assign5
+DROP DATABASE IF EXISTS guttman_202db_assign5;
+CREATE DATABASE IF NOT EXISTS guttman_202db_assign5;
+-- use command
+USE guttman_202db_assign5;
+
+-- FLUSH PRIVILEGES;
+-- end
