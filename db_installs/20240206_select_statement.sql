7170deb99aa59a60917ec41a35dee50ead8dc303
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-17 21:32:52 -0500

    table: categories

--- db_installs/20240206_select_statement.sql
+++ db_installs/20240206_select_statement.sql
@@ -4,5 +4,14 @@ CREATE DATABASE IF NOT EXISTS guttman_202db_assign5;
 -- use command
 USE guttman_202db_assign5;
 
+-- create and populate 20pts
+-- table: categories 5pts
+DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
 -- FLUSH PRIVILEGES;
 -- end
