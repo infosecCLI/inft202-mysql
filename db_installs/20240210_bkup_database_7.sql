ff3e44bba8876c908c571ee9f4420710b51bd0d7
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-18 16:57:36 -0500

    edit to enable source

--- db_installs/20240210_bkup_database_7.sql
+++ db_installs/20240210_bkup_database_7.sql
@@ -1,5 +1,4 @@
-DROP DATABASE
-IF EXISTS `guttman_202dbassign_7`;
-CREATE DATABASE
-IF NOT EXISTS `guttman_202dbassign_7`
+DROP DATABASE IF EXISTS guttman_202dbassign_7;
+CREATE DATABASE IF NOT EXISTS guttman_202dbassign_7
 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE guttman_202dbassign_7;
