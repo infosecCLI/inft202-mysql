4556949778f211a279a3bad9a2b7be4b20b578f7
usertwo <147354047+infosecCLI@users.noreply.github.com>
2024-02-24 18:53:38 -0500

    post housekeeping from drop databases

---
+++ db_installs/sqldump/guttman_202db_assign6.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14 (x86_64)
+--
+-- Host: localhost    Database: guttman_202db_assign6
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `committees`
+--
+
+DROP TABLE IF EXISTS `committees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `committees` (
+  `committee_id` int NOT NULL AUTO_INCREMENT,
+  `committee_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`committee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `committees`
+--
+
+LOCK TABLES `committees` WRITE;
+/*!40000 ALTER TABLE `committees` DISABLE KEYS */;
+INSERT INTO `committees` VALUES (1,'Governance'),(2,'Firewall Testing'),(3,'Operations'),(4,'Systems Assurance'),(5,'Infrastructure'),(6,'Corporate Advisory'),(7,'Cybersecurity'),(8,'Change Management');
+/*!40000 ALTER TABLE `committees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `members` (
+  `member_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `committee_id` int DEFAULT NULL,
+  PRIMARY KEY (`member_id`),
+  KEY `fk_committees` (`committee_id`),
+  CONSTRAINT `fk_committees` FOREIGN KEY (`committee_id`) REFERENCES `committees` (`committee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `members`
+--
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (1,'Aziz',1),(2,'Doris A',2),(3,'Doris Q',3),(4,'Peter',7),(5,'Devin',1),(6,'Jose',1),(7,'Jorge',2),(8,'Cece',8),(9,'Kevin',5),(10,'Kani',7),(11,'Zakeem',2),(12,'Thiha',1),(13,'Jason',2),(14,'Chantel',2),(15,'Christina',2),(16,'Chianti',1),(17,'Cynthia',2),(18,'Kyle',3),(19,'Cheri',NULL),(20,'Max',NULL),(21,'Roberta',NULL);
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-24 18:43:43
